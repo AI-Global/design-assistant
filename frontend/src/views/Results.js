b0b77dcc0c76cfa16016914d037625747f40375b
Micheal <mlnguyen@ualberta.ca>
2020-10-04 22:47:31 -0600

    Added fontawesome react package

--- frontend/src/views/Results.js
+++ frontend/src/views/Results.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "font-awesome/css/font-awesome.css"
-import { Tabs, Tab } from 'react-bootstrap'
-import "../Themes.css"
+import { Tabs, Tab, Table, Button} from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faCircle  } from '@fortawesome/free-regular-svg-icons'
+
 
 export default class Results extends Component {
+
+
+      
     render(){
+        var unselected = "#dee2e6";
         return (
             <main id="wb-cont" role="main" property="mainContentOfPage" class="container" style={{paddingBottom: "1rem"}}>
                 <div class="results">
@@ -13,98 +20,97 @@ export default class Results extends Component {
                 </div>
                 <Tabs>
                     <Tab eventKey="score" title="Score">
-                    <div class="table-responsive">
-                            <table id="scoreTable" role="table" class="table b-table score table-bordered">
-                                <thead class="">
-                                    <tr class="">
-                                        <th scope="col" class="">
+                    <div class="table-responsive mt-3">
+                            <Table striped bordered hover responsive> 
+                                <thead>
+                                    <tr>
+                                        <th>
                                             Dimensions
                                         </th>
-                                        <th scope="col" class="">
+                                        <th>
                                             Needs to improve
                                         </th>
-                                        <th scope="col" class="">
+                                        <th>
                                             Acceptable
                                         </th>
-                                        <th scope="col" class="">
+                                        <th>
                                             Proficient
                                         </th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr class="">
-                                        <th scope="row" class="">
+                                    <tr>
+                                        <th scope="row">
                                             Accountability
                                         </th>
                                         <td class="text-center">
-                                            <i class="fas fa-check-circle"></i>
+                                            <FontAwesomeIcon icon={faCheckCircle} size="lg" />
                                         </td>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-circle"/>
+                                            <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                         </td>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-circle"/>
+                                            <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                         </td>
                                     </tr>
                                     <tr class="">
-                                        <th scope="row" class="">
+                                        <th scope="row">
                                             Explainability
                                         </th>
                                     <td class="text-center">
-                                        <i class="far fa-lg fa-check-circle"/>
+                                        <FontAwesomeIcon icon={faCheckCircle} size="lg" />
                                     </td>
                                     <td class="text-center">
-                                        <i class="far fa-lg fa-circle">
-                                        </i>
+                                        <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                     </td>
                                     <td class="text-center">
-                                        <i class="far fa-lg fa-circle"/>
+                                        <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                     </td>
                                     </tr>
                                     <tr>
-                                        <th scope="row" class="">
+                                        <th scope="row">
                                             Data quality and rights
                                         </th>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-check-circle"/>
+                                            <FontAwesomeIcon icon={faCheckCircle} size="lg" />
                                         </td>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-circle"/>
+                                            <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                         </td>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-circle"/>
+                                            <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <th scope="row" class="">
+                                        <th scope="row">
                                             Bias and fairness
                                         </th>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-check-circle"/>
+                                            <FontAwesomeIcon icon={faCheckCircle} size="lg" />
                                         </td>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-circle"/>
+                                            <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                         </td>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-circle"/>
+                                            <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                         </td>
                                     </tr>
                                     <tr class="">
-                                        <th scope="row" class="">
+                                        <th scope="row">
                                             Robustness
                                         </th>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-check-circle"/>
+                                            <FontAwesomeIcon icon={faCheckCircle} size="lg" />
                                         </td>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-circle"/>
+                                            <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                         </td>
                                         <td class="text-center">
-                                            <i class="far fa-lg fa-circle"/>
+                                            <FontAwesomeIcon icon={faCircle} size="lg" color={unselected}/>
                                         </td>
                                     </tr>
                                </tbody>
-                            </table>
+                            </Table>
                         </div>
                     </Tab>
                     <Tab eventKey="report-card" title="Report Card">
@@ -151,6 +157,27 @@ export default class Results extends Component {
                         </div>
                     </Tab>
                 </Tabs>
+                <p>
+                    As‌ ‌AI‌ ‌continues‌ ‌to‌ ‌evolve‌ ‌so‌ ‌will‌ ‌the‌ ‌Design‌ ‌Assistant.‌ ‌
+                    We‌ ‌are‌ ‌working‌ ‌now‌ ‌to‌ ‌add‌ questions‌ ‌that‌ ‌are‌ ‌more‌ ‌industry‌ ‌specific‌ ‌and‌ ‌tailored‌ ‌for‌ ‌your‌ ‌location.‌ 
+                    ‌To‌ ‌do‌ ‌this,‌ ‌we‌ can‌ ‌use‌ ‌your‌ ‌help!‌ ‌Share‌ ‌with‌ ‌us‌ ‌the‌ ‌results‌ ‌of‌ ‌your‌ ‌report.‌ 
+                    ‌Let‌ ‌us‌ ‌know‌ ‌where‌ ‌you‌ ‌need‌ more‌ ‌clarification,‌ ‌and‌ ‌where‌ ‌more‌ ‌guidance‌ ‌might‌ ‌be‌ ‌needed.‌
+                     If‌ ‌you‌ ‌weren’t‌ ‌ready‌ ‌to‌ ‌answer‌ ‌all‌ ‌of‌ ‌the‌ ‌questions‌ ‌today,‌ ‌that’s‌ ‌ok,‌ ‌save‌ ‌your‌ ‌report,‌ ‌and‌ you‌ ‌can‌ ‌reload‌ ‌it‌ ‌the‌ ‌next‌ ‌time‌ ‌you‌ ‌return.‌
+                </p>
+                <p>
+                    As‌ ‌an‌ ‌open‌ ‌source‌ ‌tool,‌ ‌we‌ ‌will‌ ‌continue‌ ‌to‌ ‌adjust‌ ‌quickly‌ ‌based‌ ‌on‌ ‌our‌ ‌communities‌ needs.‌ 
+                    ‌Please‌ ‌let‌ ‌us‌ ‌know‌ ‌if‌ ‌you‌ ‌find‌ ‌any‌ ‌issues‌ ‌and‌ ‌we‌ ‌will‌ ‌be‌ ‌happy‌ ‌to‌ ‌update!‌
+                </p>
+                <p>
+                    If‌ ‌you‌ ‌are‌ ‌wondering‌ ‌what‌ ‌to‌ ‌do‌ ‌with‌ ‌your‌ ‌results,‌ ‌and‌ ‌how‌ ‌you‌ ‌can‌ ‌improve,‌ ‌check‌ ‌out‌ the Responsible AI Design Assistant Guide ‌that‌ ‌includes‌ ‌definitions‌ ‌and‌ ‌lots‌ ‌of‌ additional‌ ‌information.‌ ‌
+                    If‌ ‌you‌ ‌are‌ ‌in‌ ‌need‌ ‌of‌ ‌additional‌ ‌support,‌ ‌contact‌ ‌us,‌ ‌and‌ ‌we‌ ‌can‌ put‌ ‌you‌ ‌in‌ ‌touch‌ ‌with‌ ‌a‌ ‌trusted‌ ‌service‌ ‌provider.‌
+                </p>
+                <p>
+                    Since‌ ‌we‌ ‌want‌ ‌you‌ ‌to‌ ‌use‌ ‌the‌ ‌Design‌ ‌Assistant‌ ‌early‌ ‌and‌ ‌often,‌ ‌you‌ ‌can‌ ‌click‌ ‌the‌ ‌button‌ below‌ ‌to‌ ‌start‌ ‌over‌ ‌again!‌
+                </p>
+                <Link to='/'>
+                    <Button>Start Again</Button>
+                </Link>
             </main>
         );
     }
