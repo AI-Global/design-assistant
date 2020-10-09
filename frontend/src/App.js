923313d70af3c5689531ca864991254d99d5dd98
Dalton Ronan <dronan@ualberta.ca>
2020-10-09 06:36:34 -0600

    clear survey on reset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
   }
 
   resetSurvey() {
-    console.log('RESET')
+    model.clear()
     this.handleCloseModal()
     this.setState({ isSurveyStarted: false })
   }
