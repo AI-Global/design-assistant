0f409b5bca6a69e9cc06c8d1a895bb3baa1852a2
Dalton Ronan <dronan@ualberta.ca>
2020-10-09 01:58:03 -0600

    attempt to refactor views and react components; I think this should work better

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App isSurverStarted={false}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
