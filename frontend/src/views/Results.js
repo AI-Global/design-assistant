218a78dfb2dbd595908fb86edbae2a536c0588ad
Micheal <mlnguyen@ualberta.ca>
2020-10-12 22:22:44 -0600

    Add Export to PDF functionality

--- frontend/src/views/Results.js
+++ frontend/src/views/Results.js
@@ -5,15 +5,16 @@ import "font-awesome/css/font-awesome.css"
 import { Tabs, Tab, Table, Button, Nav } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle, faCircle } from '@fortawesome/free-regular-svg-icons'
-import "../css/theme.css"
-import "../css/survey.css"
+import "../css/results.css"
 import { ResponsiveRadar } from "@nivo/radar";
+import {jsPDF} from "jspdf";
+import {autoTable} from "jspdf-autotable";
 
 const Dimensions = {
     Accountability: {label: "A", name: "Accountability"},
-    Bias: {label: "B", name: "Bias and fairness"},
-    Data: {label: "D", name: "Data quality and rights"},
     Explainability: {label: "EI", name: "Explainability"},
+    Data: {label: "D", name: "Data quality and rights"},
+    Bias: {label: "B", name: "Bias and fairness"},
     Robustness: {label: "R", name: "Robustness"},
 }
 
@@ -26,29 +27,272 @@ const QuestionTypes =
 var radarChartData = [];
 
 export default class Results extends Component {
+
+
+    addHeader(doc, y){
+        const title = "Responsible AI Design Report Card";
+        const img = new Image();
+        img.src = '../img/aiglobal-logo.jpg';
+        const iw = 1403, ih = 507, ratio = 0.025;
+        doc.addImage(img, 'jpg', 10, 10, Math.floor(iw * ratio), Math.floor(ih * ratio));
+        doc.setFontSize(24);
+        doc.text(title, doc.internal.pageSize.getWidth() / 2, y, null, null, "center");
+        y += doc.getTextDimensions(title).h + 5; // padding
+        return y;
+    }
+    addAbout(doc, y) {
+        const title = "About the Design Assistant";
+        doc.setFontSize(16);
+        doc.text(title, 10, y);
+        y += 1; // Padding between title and horizontal line.
+        doc.setDrawColor(0, 0, 0);
+        doc.line(10, y, 200, y);  // Horizontal line.
+        y += doc.getTextDimensions(title).h + 1; // Padding between title.
+        doc.setFontSize(10);
+        // Custom add text function.
+        const addText = (text, x, y) => {
+          const length = 118;
+          const words = text.split(" ");
+          let tmpY = y;
+          let line = "";
+          const add = () => {
+            doc.text(line, x, tmpY);
+            tmpY += doc.getTextDimensions(line).h + 1;
+            line = "";
+          };
+          const addLink = (tmp) => {
+            let match;
+            do {
+                let match = /(\[(?:\[??[^[]*?\]))(\((?:\(??[^(]*?\)))/g.exec(tmp);
+                if (match) {
+                  const text = match[1].substring(1, match[1].length - 1);
+                  const newTmp = tmp.replace(match[0], text);
+                  if (newTmp.length >= length) {
+                    break;
+                  }
+                  tmp = newTmp;
+                  const url = match[2].substring(1, match[2].length - 1);
+                  doc.textWithLink(text, x + doc.getTextDimensions(tmp.substring(0, tmp.indexOf(text))).w, tmpY, { url: url });
+                }
+            } while (match);
+            return tmp;
+          };
+          let tmpBeforeLink = "";
+          let linkFound = false;
+          let index = -1;
+          for (let i = 0; i < words.length; i++) {
+            let tmp = line;
+            if (words[i].charAt(0) === "[") {
+              tmpBeforeLink = tmp;
+              linkFound = true;
+              index = i - 1;
+            }
+            if (words[i].charAt(words[i].length - 1) === ")") {
+              linkFound = false;
+            }
+            tmp += words[i];
+            if (i < words.length - 1) {
+              tmp += " ";
+            }
+            tmp = addLink(tmp);
+            if (tmp.length >= length) {
+              if (linkFound) {
+                i = index;
+                line = tmpBeforeLink;
+                linkFound = false;
+              } else {
+                i--;
+              }
+              add();
+            } else {
+              line = tmp;
+            }
+          }
+          if (line !== "") {
+            line = addLink(line);
+            add();
+          }
+          return tmpY - y;
+        };
+        // Add about text to pdf.
+        const text = [
+          "As AI continues to evolve so will the Design Assistant. We are working now to add questions that are more industry specific and tailored for your location. To do this, we can use your help! Share with us the results of your report. Let us know where you need more clarification, and where more guidance might be needed. If you weren't ready to answer all of the questions today, that's ok, save your report, and you can reload it the next time you return.",
+          "As an open source tool, we will continue to adjust quickly based on our communities needs. Please let us know if you find any issues and we will be happy to update!",
+          "If you are wondering what to do with your results, and how you can improve, check out the [Responsible AI Design Assistant Guide](https://docs.google.com/document/d/1eEMZkY139xO557Tv8rV7zlahycZUdkarr-YJftwkU20/edit#heading=h.l9n4rt31jkfh) that includes definitions and lots of additional information. If you are in need of additional support, contact us, and we can put you in touch with a trusted service provider.",
+          "Since we want you to use the Design Assistant early and often, you can click the button below to start over again!"
+        ];
+        for (let i = 0; i < text.length; i++) {
+          y += addText(text[i], 15, y);
+          if (i < text.length - 1) {
+            y += 2;
+          }
+        }
+        y += 10;
+        return y;
+    }
+    addScore(doc, y) {
+        const checkCircle = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAoAAAAKABXX67owAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAHfSURBVDiNldTPb01REAfwT5s+RFpEYtPyamXVVztBJMSGjd/8AVLpRizaf0HKwk4sxY/uRMRKEQn9E7T8B3hLBO1rRDyLMzf39r17b/Wb3Jw7c2a+Z87MmRlQjxGcxgRGQ/cFH/EaPzfw78M45rGGbsW3hsdolhEMlOimcRdbQ17CIj6FvA8ncDDkNdzA/bpIbxcieoFWjW0rbDL7uSrD6TD4g5m603swGz5dTPVu7pfnczOkGWbCt6Mn5/Py628G2wv/C8HxMFOMyKOty2kvDuEbboU8WYh6GK6E4v0mSA/je4Fod+iXQndpUHr88O4/SY9IzbFTuukFfI29xVhbg9Z3VIYJ3MGWHtKjeIUdEen5kDN8jnV0qCSiXXiLPTggpeo3jkkFGgnSc3hTda1BtON/b6w/5Gk5iyc4iZdBuoozFaQZRxsu6y/eEJ7Ku+pvrCtxSBWWw+4i1c+tgWcF8l/SjKhC33ODR8obpIHn0ng8XkNK3iAPisrxOKmspRsY24B0NnxXpem3DtfkQ2h2A6Je0mwIXa0ympPndEHKWxUm5dfv4mZxs2zQT+EetoW8bP2gb0pFzArdwXWF4VOHplTQLO9lX0cqVF9OqyIuYhinIrqxIGzjg9TKK1WO/wBzvo3rvEdWdAAAAABJRU5ErkJggg==";
+        const circle = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAXCAYAAADgKtSgAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAqAAAAKgBefSzxgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAGoSURBVEiJrdXLThRBFAbgj3bYsVEYXCgTFmoyE4kujRslPII+A8o7kIkP5EPoxstGNIrXCRth4Q1BlkrGRVVlKk332M7wJ53qU1XnP1V1qv4z49+4htu4iLP4ic94jFcN/E9gFhvYwXDMt4P7aFWRzFT09fAQ3Wgf4wkG+IpFXMJNnIlz3uFObGuxhsO4qkNsYr5m7gL6+BXnH0T/SvQy4hdYGreKDB1sZQG65QmzeJsRzzUkTpjLAmwr5WDD6CiarriMjtER3csH0q3YnJA4oR95Bqnjeuz4IyRpGszjd+RbKXArDjzF9ynJf+B5/F8thJcHH6YkTvgY2wsFzkVj/5TI0+4XCmEr1D+W/0U7tt8K7EXjyimRJ549guql29Ku82iIxcgzxNXUme55f0ryB0r3nCCbQ+GFdSYkXsZR5FnPB1pG2rJlMm15Gf3fqND3rqBqKUDTHSxnxJWqmLCWBTgSclCX5LZwxukoTuh5VSXqCpWoF+1jPMMnfMF5XMYNo0q0jbt4X7fqHC1BNgfG19CBkLzGNbSMFaH6LwnVfx+7eITX4xz/ApJjdwVAbpi0AAAAAElFTkSuQmCC";
+        const title = "Results Overview";
+        doc.setFontSize(16);
+        doc.text(title, 10, y);
+        y += 1; // Padding between title and horizontal line.
+        doc.setDrawColor(0, 0, 0);
+        doc.line(10, y, 200, y);  // Horizontal line.
+        y += doc.getTextDimensions(title).h + 1; // Padding between title.
+        // Base64 FontAwesomes icons.
+        // Add table to pdf. 
+        doc.autoTable( {
+            startY: y,
+
+            html: '#score',
+            didParseCell: (data) => {
+              // Style cells.
+              if (data.section === "head") {
+                data.cell.styles.textColor = "#000000";
+                if (data.cell.text[0] === "Dimensions") {
+                  data.cell.styles.fillColor = "#BFBFBF";
+                } else {
+                  data.cell.styles.fillColor = "#D0E0E3";
+                }
+              } else if (data.section === "body" && data.column.index === 0) {
+                data.cell.styles.fontStyle = "bold";
+                data.cell.styles.textColor = "#000000";
+              }
+            },
+            didDrawCell: (data) => {
+              // Draw cell border
+              doc.setDrawColor(222, 226, 230);
+              doc.line(data.cell.x, data.cell.y, data.cell.x + data.column.width, data.cell.y);
+              doc.line(data.cell.x, data.cell.y, data.cell.x, data.cell.y + data.row.height);
+              doc.line(data.cell.x + data.column.width, data.cell.y, data.cell.x + data.column.width, data.cell.y + data.row.height);
+              doc.line(data.cell.x, data.cell.y + data.row.height, data.cell.x + data.column.width, data.cell.y + data.row.height);
+              // Add images
+              const td = data.cell.raw;
+              const img = td.getElementsByTagName('svg')[0];
+              if (img !== undefined) {
+                const dim = data.cell.height - data.cell.padding('vertical');
+                const textPos = data.cursor;
+                if (img.classList.value.indexOf('fa-check-circle') !== -1) {
+                  doc.addImage(checkCircle, textPos.x + data.cell.width / 2 - dim, textPos.y, dim, dim)
+                } else if (img.classList.value.indexOf('fa-circle') !== -1) {
+                  doc.addImage(circle, textPos.x + data.cell.width / 2 - dim, textPos.y, dim, dim)
+                }
+              }
+            }
+        });
+        y += 60;
+        return y;
+    }
+    addReportCard(doc, y) {
+        const title = "Detailed Report";
+        doc.setFontSize(16);
+        doc.text(title, 10, y);
+        y += 1; // Padding between title and horizontal line.
+        doc.setDrawColor(0, 0, 0);
+        doc.line(10, y, 200, y);  // Horizontal line.
+        y += doc.getTextDimensions(title).h + 2; // Padding between title.
+        // Base64 FontAwesomes icons.
+        // Add table to pdf. 
+        for (let name in Dimensions){
+            console.log(Dimensions[name].label)
+            doc.setFontSize(14);
+            doc.text(Dimensions[name].name, 12, y);
+            y += doc.getTextDimensions(name).h;
+            doc.autoTable( {
+                startY: y,
+
+                html: '#report-card-' + Dimensions[name].label,
+                columnStyles: {
+                    0: {cellWidth: 60},
+                    1: {cellWidth: 60},
+                    2: {cellWidth: 60},
+                },
+                didParseCell: (data) => {
+                // Style cells.
+                data.cell.styles.textColor = "#000000";
+                if (data.section === "head") {
+                    data.cell.styles.fillColor = "#D0E0E3";
+                }
+                },
+                didDrawCell: (data) => {
+                    // Draw cell border.
+                    doc.setDrawColor(222, 226, 230);
+                    doc.line(data.cell.x, data.cell.y, data.cell.x + data.column.width, data.cell.y);
+                    doc.line(data.cell.x, data.cell.y, data.cell.x, data.cell.y + data.row.height);
+                    doc.line(data.cell.x + data.column.width, data.cell.y, data.cell.x + data.column.width, data.cell.y + data.row.height);
+                    doc.line(data.cell.x, data.cell.y + data.row.height, data.cell.x + data.column.width, data.cell.y + data.row.height);
+                },
+                didDrawPage: (data) => {
+                // Reset y position.
+                    doc.pageNumber++;
+                    y = 20;
+                }
+            });
+            y += doc.previousAutoTable.finalY;
+        }
+        return y;
+    }
+
+    exportReport(){
+        const doc = new jsPDF();
+        var y = 35;
+        y = this.addHeader(doc, y);
+        y = this.addAbout(doc, y);
+        y = this.addScore(doc, y);
+        y = this.addReportCard(doc, y);
+        doc.save('Responsible AI Design Report Card.pdf')   
+    }
+
+
+
+
     DisplayQuestion(result, question){     
         var choices = question?.choices?.filter((choice) => result.includes(choice?.value));     
         return (
             <tr key={question?.name}>
                 <td>
-                    {question?.title?.default}
-                </td>
-                <td>
-                    {choices.map(choice => {
-                        return (<p key={choice?.value}>{choice?.text?.default}</p>)
+                    {question?.title?.default.split("\n").map(function(item, idx) {
+                        return (
+                            <span key={idx}>
+                                {item}
+                                <br/>
+                            </span>
+                        )
                     })}
                 </td>
                 <td>
-                    {question?.recommendation?.default}
+                    {choices.map(choice => {
+                        return (
+                            choice?.text?.default.split("\n").map(function(item, idx) {
+                                return (
+                                    <span key={idx}>
+                                        {item}
+                                        <br/>
+                                    </span>
+                                )
+                            })
+                        )
+                    })}
+                </td>
+                <td>
+                    {question?.recommendation?.default.split("\n").map(function(item, idx) {
+                        return (
+                            <span key={idx}>
+                                {item}
+                                <br/>
+                            </span>
+                        )
+                    })}
                 </td>
             </tr>
         )
     }
 
-    CreateReportCard(results, questions){
+    CreateReportCard(dimension, results, questions){
         return (
             <div className="report-card mt-3">
-                <Table bordered responsive className="report-card-table">
+                <Table id={"report-card-"+ dimension} bordered responsive className="report-card-table">
                     <thead>
                         <tr role="row">
                             <th role="columnheader" scope="col" className="report-card-headers">
@@ -140,8 +384,10 @@ export default class Results extends Component {
     }
 
     render() {
+        console.log(this);
         var json = this.props.location.state;
         var surveyResults = {"QBAEDF":"AI Global Project","QA02D4":"This is the AI Global Project","QD85E8":"itemA96E95","Q836F4":"itemA47434","QA0F6A":["itemAA35A8"],"Q4435F":"itemAED0BB","Q71FC3":["itemA14629","itemAFDC95","itemADA33F","itemA74430","itemA4C27A"],"Q1C8CF":["itemA7DF83","itemA9CF08","itemA9E317","itemAB26CF","itemAC637B"],"Q56654":"itemAC210D","Q22F17":["itemA89E02","itemA83E75","itemAB015E","itemA1EC52","itemA433A7","itemA6A457","itemA30DB7","itemA1AE82","itemA7C3E5","itemAE6FAD","itemAA93BD","itemA76B32"],"Q1577E":["itemAD1BDC","itemA90EF1","itemA7CDEE","itemA44372","itemA01B9E","itemA2B090","itemAA34FF","itemAACFA1","itemA44302"],"QAF269":["itemA30473","itemA4FD04"],"Q4805F":["itemA24402"],"QD3651":"itemABBD06","QC9FE9":"itemA34F49","QC8371":"itemA960F9"}
+
         var pages = json["pages"];
         var allQuestions = [];
         pages.map(page => {
@@ -158,10 +404,11 @@ export default class Results extends Component {
                 <h1 className="section-header">
                     Results
                 </h1>
+                <button id="saveButton" type="button" className="btn btn-save mr-2 btn btn-primary export-button" onClick={this.exportReport.bind(this)}>Export</button>
                 <Tabs defaultActiveKey="score">
                     <Tab eventKey="score" title="Score">
                         <div className="table-responsive mt-3">
-                            <Table bordered hover responsive className="report-card-table">
+                            <Table id="score" bordered hover responsive className="report-card-table">
                                 <thead>
                                     <tr>
                                         <th className="score-card-dheader">
@@ -193,19 +440,19 @@ export default class Results extends Component {
                             <Tab.Container id="left-tabs-example" defaultActiveKey="accountability">
                                 <Tab.Content>
                                     <Tab.Pane eventKey="accountability" >
-                                        {this.CreateReportCard(surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Accountability.label ))}
+                                        {this.CreateReportCard(Dimensions.Accountability.label, surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Accountability.label ))}
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="explainability">
-                                        {this.CreateReportCard(surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Explainability.label ))}                               
+                                        {this.CreateReportCard(Dimensions.Explainability.label, surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Explainability.label ))}                               
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="data-quality">     
-                                        {this.CreateReportCard(surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Data.label ))}                              
+                                        {this.CreateReportCard(Dimensions.Data.label, surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Data.label ))}                              
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="bias-fairness">
-                                        {this.CreateReportCard(surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Bias.label ))}                                 
+                                        {this.CreateReportCard(Dimensions.Bias.label, surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Bias.label ))}                                 
                                     </Tab.Pane>            
                                     <Tab.Pane eventKey="robustness">
-                                        {this.CreateReportCard(surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Robustness.label ))}                                   
+                                        {this.CreateReportCard(Dimensions.Robustness.label, surveyResults, questions.filter(x => x.score?.dimension === Dimensions.Robustness.label ))}                                   
                                     </Tab.Pane>                                                                 
                                 </Tab.Content>
                                 <Nav variant="tabs" className="report-card-nav" defaultActiveKey="accountability">
