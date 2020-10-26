a7c43af6c2ee64c6339e4b25a1e28853de5f9eaa
Micheal <mlnguyen@ualberta.ca>
2020-10-25 22:19:19 -0600

    Added Authentication Token for Users

---
+++ backend/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const jwtSecret = process.env.JWT_SECRET;
+
+function auth(req, res, next) {
+    try {
+        const token = req.header('x-auth-token');
+
+        if(!token) {
+            res.status(401).json({ msg: 'Authorization denied, token missing' })
+        }
+
+        const verified = jwt.verify(token, jwtSecret);
+        
+        req.user = verified;
+        next();
+    }
+    catch(e) {
+        console.log(e);
+        res.status(400).json({ msg: 'Invalid Token' })
+    }
+}
+
+module.exports = auth;
