57fcf139f51ed1c205e862c9e38fa4282b16efbf
Colin Choi <cechoi@ualberta.ca>
2020-11-01 00:26:49 -0600

    Add other question to tombstone questions

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -131,12 +131,14 @@ function createPages(q, Dimensions) {
 
     for (let question of q) {
         if (question.questionType == "tombstone") {
-            tombQuestions["tombstone"].push(question)//tombstone.push(question);
+            tombQuestions["tombstone"].push(question);
         } else if (question.trustIndexDimension) {
             dimQuestions[question.trustIndexDimension].push(question)
         }
     }
 
+    // Add Other question to tombstone and create page 
+    tombQuestions["tombstone"].push({ responseType: "comment", id: "otherTombstone", question: "Other:", alttext: "If possible, support the feedback with specific recommendations \/ suggestions to improve the tool. Feedback can include:\n - Refinement to existing questions, like suggestions on how questions can be simplified or clarified further\n - Additions of new questions for specific scenarios that may be missed\n - Feedback on whether the listed AI risk domains are fulsome and complete\n - What types of response indicators should be included for your context?" });
     projectDetails = createPage(tombQuestions["tombstone"], "projectDetails1", "Project Details", Dimensions);
     page.pages.push(projectDetails);
 
