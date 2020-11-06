9894e8e09eaa5ea02723bdfdb7f06d196b74c2ba
Erik <ligai@ualberta.ca>
2020-11-06 02:16:39 -0700

    fixed small error

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user.model');
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
+const { json } = require('express');
 const auth = require('../middleware/auth');
 
 const jwtSecret = process.env.JWT_SECRET;
