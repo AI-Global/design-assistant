a87c1fdcded790ef7184efd62fb02e96a5ffab49
Micheal <mlnguyen@ualberta.ca>
2020-11-12 23:30:02 -0700

    created mail service using nodemailer

---
+++ backend/middleware/mailService.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+nodemailer = require('nodemailer');
+fs = require('fs');
+
+//const sender = "<noreply@ai-global.org>"
+const mailServiceSender = process.env.MAILSERVICE_USERNAME;
+const mailServicePassword = process.env.MAILSERVICE_PASSWORD;
+
+async function sendEmail(email, emailSubject, emailTemplate) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: mailServiceSender, 
+          pass: mailServicePassword, 
+        }
+    });
+
+    fs.readFile(emailTemplate, {encoding: 'utf-8'}, function (err, html){
+        if(err){
+            console.log(err);
+        }
+        else{
+            let info = transporter.sendMail({
+                from: mailServiceSender, // sender address
+                to: email, // receiver
+                subject: emailSubject, // Subject line
+                html: html
+            });
+            console.log("Message sent: %s", info.messageId);
+        }
+    })
+}
+
+exports.sendEmail = sendEmail;
