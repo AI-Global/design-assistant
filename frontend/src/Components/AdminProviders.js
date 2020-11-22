83a8fb9a411b88933aff09be6e0dfd412a8ed40e
Micheal <mlnguyen@ualberta.ca>
2020-11-22 03:42:36 -0700

    create new provider functionality

--- frontend/src/Components/AdminProviders.js
+++ frontend/src/Components/AdminProviders.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import { Table, Button, Modal, Form, Col } from 'react-bootstrap';
-
+import IconButton from '@material-ui/core/IconButton';
+import Add from '@material-ui/icons/Add';
 export default class AdminProviders extends Component {
     constructor(props) {
         super(props);
@@ -40,7 +41,7 @@ export default class AdminProviders extends Component {
         let form = event.target.elements;
         let trustedAIProviders = this.state.trustedAIProviders;
         let currentIndex = this.state.currentIndex;
-        let endPoint = "/trustedAIProviders/" + trustedAIProviders[currentIndex]?._id;
+        let endPoint = "/trustedAIProviders/" + (trustedAIProviders[currentIndex]?._id ?? "");
         let body = {
             resource: form.title.value,
             description: form.description.value,
@@ -49,10 +50,10 @@ export default class AdminProviders extends Component {
         axios.put(process.env.REACT_APP_SERVER_ADDR + endPoint, body)
             .then(response => {
                 let newProvider = response.data;
-                if(trustedAIProviders[currentIndex]?._id){
+                if (trustedAIProviders[currentIndex]?._id) {
                     trustedAIProviders[currentIndex] = newProvider;
                 }
-                else{
+                else {
                     trustedAIProviders.unshift(newProvider);
                 }
                 this.setState({ trustedAIProviders: trustedAIProviders });
@@ -60,14 +61,14 @@ export default class AdminProviders extends Component {
         this.setState({ showEditModal: false });
     }
 
-    render(){
-                var trustedAIProviders = this.state.trustedAIProviders;
-                var currentIndex = this.state.currentIndex;
-                const showEdit = this.state.showEditModal;
-                const handleClose = () => this.setState({ showDeleteWarning: false });
-                const handleEditClose = () => this.setState({ showEditModal: false });
-                const handleEditShow = (index) => this.setState({ showEditModal: true, currentIndex: index });
-                return(
+    render() {
+        var trustedAIProviders = this.state.trustedAIProviders;
+        var currentIndex = this.state.currentIndex;
+        const showEdit = this.state.showEditModal;
+        const handleClose = () => this.setState({ showDeleteWarning: false });
+        const handleEditClose = () => this.setState({ showEditModal: false });
+        const handleEditShow = (index) => this.setState({ showEditModal: true, currentIndex: index });
+        return (
             <div>
                 <Modal
                     centered
@@ -77,11 +78,11 @@ export default class AdminProviders extends Component {
                 >
                     <Modal.Header>
                         <Modal.Title id="contained-modal-title-vcenter">
-                        Warning!
+                            Warning!
                     </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                    Are you sure you would like to delete this Trusted AI Provider?
+                        Are you sure you would like to delete this Trusted AI Provider?
                 </Modal.Body>
                     <Modal.Footer>
                         <Button id="DeleteSurveyButton" onClick={() => this.deleteProvider()}>Yes</Button>
@@ -97,40 +98,40 @@ export default class AdminProviders extends Component {
                     keyboard={false}>
                     <Modal.Header closeButton>
                         <Modal.Title>
-                        Trusted AI Provider
+                            Trusted AI Provider
                         </Modal.Title>
-                    </Modal.Header>        
+                    </Modal.Header>
                     <Form onSubmit={(e) => this.saveProvider(e)}>
                         <Modal.Body className="p-4">
-                                <Form.Group controlId="title">
-                                    <Form.Label>Title</Form.Label>
-                                    <Form.Control type="text" placeholder="Title" required="required" autoComplete="text" isInvalid={false} aria-label="Title" defaultValue={trustedAIProviders[currentIndex]?.resource}/>
-                                    <Form.Control.Feedback type="invalid">
-                                        {}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                                <Form.Group controlId="description">
-                                    <Form.Label>Description</Form.Label>
-                                    <Form.Control as="textarea" placeholder="Description" autoComplete="text" isInvalid={false} aria-label="Description" defaultValue={trustedAIProviders[currentIndex]?.description} />
-                                    <Form.Control.Feedback type="invalid">
-                                        {}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                                <Form.Group controlId="source">
-                                    <Form.Label>Source</Form.Label>
-                                    <Form.Control type="text" placeholder="Source" required="required" autoComplete="text" isInvalid={false} aria-label="Source" defaultValue={trustedAIProviders[currentIndex]?.source}/>
-                                    <Form.Control.Feedback type="invalid">
-                                        {}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                        </Modal.Body>        
+                            <Form.Group controlId="title">
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control type="text" placeholder="Title" required="required" autoComplete="text" isInvalid={false} aria-label="Title" defaultValue={trustedAIProviders[currentIndex]?.resource} />
+                                <Form.Control.Feedback type="invalid">
+                                    { }
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group controlId="description">
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control as="textarea" placeholder="Description" autoComplete="text" isInvalid={false} aria-label="Description" defaultValue={trustedAIProviders[currentIndex]?.description} />
+                                <Form.Control.Feedback type="invalid">
+                                    { }
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group controlId="source">
+                                <Form.Label>Source</Form.Label>
+                                <Form.Control type="text" placeholder="Source" required="required" autoComplete="text" isInvalid={false} aria-label="Source" defaultValue={trustedAIProviders[currentIndex]?.source} />
+                                <Form.Control.Feedback type="invalid">
+                                    { }
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Modal.Body>
                         <Modal.Footer>
-                                <Col md={9} className="mr-4">
-                                    <Button id="resetButton" onClick={() => {this.setState({showDeleteWarning: true})}}> Delete</Button>
-                                </Col>
-                                <Col md={2}>
-                                    <Button id="saveButton" type="submit" label="save" >Save</Button>
-                                </Col>
+                            <Col md={9} className="mr-4">
+                                <Button id="resetButton" onClick={() => { this.setState({ showDeleteWarning: true }) }}> Delete</Button>
+                            </Col>
+                            <Col md={2}>
+                                <Button id="saveButton" type="submit" label="save" >Save</Button>
+                            </Col>
                         </Modal.Footer>
                     </Form>
                 </Modal>
@@ -140,7 +141,9 @@ export default class AdminProviders extends Component {
                         <tr>
                             <th width="33%">Trusted AI Provider</th>
                             <th>Description</th>
-                            <th></th>
+                            <th className="text-center">
+                                <IconButton aria-label="add provider" size="small" onClick={() => { handleEditShow(-1) }}><Add /></IconButton>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
@@ -149,7 +152,7 @@ export default class AdminProviders extends Component {
                                 <tr key={index}>
                                     <td><a href={provider?.source}>{provider?.resource}</a></td>
                                     <td>{provider?.description}</td>
-                                    <td><Button onClick={() => {handleEditShow(index)}}>Edit</Button></td>
+                                    <td><Button onClick={() => { handleEditShow(index) }}>Edit</Button></td>
                                 </tr>
                             )
                         })}
