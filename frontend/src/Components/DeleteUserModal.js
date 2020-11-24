a19b7d7126d52b5dc5f0a740e82e65fffcb38d20
Harrison Peters <hmp@ualberta.ca>
2020-11-24 11:48:54 -0700

    adjusted deleteUserModal

--- frontend/src/Components/DeleteUserModal.js
+++ frontend/src/Components/DeleteUserModal.js
@@ -17,23 +17,25 @@ export default function DeleteUserModal(props) {
             centered
             show={props.show}
             onHide={props.onHide}>
-            <Modal.Header>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Delete {props.user?.username}'s Submissions?
-              </Modal.Title>
-            </Modal.Header>
             <Modal.Body>
                 <Form>
                     <Form.Group>
-                        <div key='inline-radio' className="mb-3" style={{ paddingTop: "1.5em" }}>
-                            <Form.Check inline label="Yes" type='radio' id='inline-radio-1' checked={deleteSubmissions} onChange={() => setDeleteSubmissions(true)} />
-                            <Form.Check inline label="No" type='radio' id='inline-radio-2' checked={!deleteSubmissions} onChange={() => setDeleteSubmissions(false)} />
+                        <div class="container">
+                            <div class="row">
+                                <div class="col text-center">
+                                <Form.Label> Delete {props.user?.username}'s Submissions?</Form.Label>
+                                    <div key='inline-radio' className="mb-3" style={{ paddingTop: "1.5em" }}>
+                                        <Form.Check inline label="Yes" type='radio' id='inline-radio-1' checked={deleteSubmissions} onChange={() => setDeleteSubmissions(true)} />
+                                        <Form.Check inline label="No" type='radio' id='inline-radio-2' checked={!deleteSubmissions} onChange={() => setDeleteSubmissions(false)} />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </Form.Group>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-            <Button onClick={() => props.onHide()} id="resetButton">Cancel</Button>
+                <Button onClick={() => props.onHide()} id="resetButton">Cancel</Button>
                 <Button onClick={() => props.confirmDelete(deleteSubmissions)} id="saveButton">Confirm</Button>
             </Modal.Footer>
         </Modal>
