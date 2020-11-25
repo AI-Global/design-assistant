f350ceb6725be702960f10a8e9c687b421cf6964
Erik <ligai@ualberta.ca>
2020-11-24 21:35:38 -0700

    error check for empty database modal

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -371,7 +371,7 @@ async function createPages(q, filters) {
 router.get('/', async (req, res) => {
     // Optional filters in req body
     filters = req.query;
-    // Only request parent questions from DB
+    //Only request parent questions from DB
     Question.find({ "child": false })
         .sort({ questionNumber: 1 })
         .then(async (questions) => {
@@ -379,6 +379,16 @@ router.get('/', async (req, res) => {
             res.status(200).send(pages);
         })
         .catch((err) => res.status(400).send(err));
+
+    // Test query for empty database
+    // Question.find({"questionNumber": 0})
+    //     .then(async (questions) => {
+    //         console.log(questions);
+    //         pages = await createPages(questions, filters);
+    //         console.log(pages);
+    //         res.status(200).send(pages);            
+    //     })
+    //     .catch((err) => res.status(400).send(err));
 
 });
 
