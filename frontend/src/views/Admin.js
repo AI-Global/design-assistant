652ce20cd45436b00a3d89afbfba40ee0cc5e351
Colin Choi <cechoi@ualberta.ca>
2020-11-25 12:21:28 -0700

    swap role and organization columns

--- frontend/src/views/Admin.js
+++ frontend/src/views/Admin.js
@@ -31,6 +31,7 @@ const User = props => (
     <TableRow hover={true}>
         <TableCell style={{ textAlign: "center" }}>{props.user.email}</TableCell>
         <TableCell style={{ textAlign: "center" }}>{props.user.username}</TableCell>
+        <TableCell style={{ textAlign: "center" }}>{props.user?.organization}</TableCell>
         <TableCell style={{ textAlign: "center" }}>
 
             {(props.role === "superadmin") ?
@@ -43,7 +44,6 @@ const User = props => (
                 : props.user.role}
 
         </TableCell>
-        <TableCell style={{ textAlign: "center" }}>{props.user?.organization}</TableCell>
         <TableCell align="center">
             <Button size="sm" onClick={() => props.nextPath("/ViewSubmissions/" + props.user._id)}>View</Button>
         </TableCell>
@@ -225,7 +225,7 @@ export default class AdminPanel extends Component {
         let form = event.target.elements;
         let orgFilter = form.orgFilter.value;
         let roleFilter = form.roleFilter.value;
-        this.setState({ orgFilter: orgFilter, roleFilter: roleFilter, usersPage:0, submissionsPage:0 });
+        this.setState({ orgFilter: orgFilter, roleFilter: roleFilter, usersPage: 0, submissionsPage: 0 });
     }
 
     handleSubmissionFilters(event) {
@@ -233,7 +233,7 @@ export default class AdminPanel extends Component {
         let form = event.target.elements;
         let userFilter = form.userFilter.value;
         let projectFilter = form.projectNameFilter.value;
-        this.setState({ userFilter: userFilter, projectFilter: projectFilter, usersPage:0, submissionsPage:0 });
+        this.setState({ userFilter: userFilter, projectFilter: projectFilter, usersPage: 0, submissionsPage: 0 });
     }
 
     showDeleteUserModal(user) {
@@ -386,10 +386,10 @@ export default class AdminPanel extends Component {
                                                 Name
                                         </TableCell>
                                             <TableCell className="score-card-headers" style={{ textAlign: "center" }}>
-                                                Role
+                                                Organization
                                         </TableCell>
                                             <TableCell className="score-card-headers" style={{ textAlign: "center" }}>
-                                                Organization
+                                                Role
                                         </TableCell>
                                             <TableCell className="score-card-headers" style={{ textAlign: "center" }}>
                                                 Submissions
