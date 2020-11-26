fad1014d5bc773281d0c7e49588bfc6aa9585147
Micheal <mlnguyen@ualberta.ca>
2020-11-25 22:07:22 -0700

    added home page integration tests (selenium)

---
+++ frontend/src/integration_tests/HomePage.test.js
@@ -0,0 +1,63 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+require('dotenv').config();
+
+async function setUpDriver() {
+    let driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new chrome.Options().headless())
+        .build();
+    await driver.get(process.env.REACT_APP_TESTING_ADDR);
+    return driver;
+}
+
+test('Successfully open User Registration page', async () => {
+    let driver = await setUpDriver();
+    await driver.findElement(By.className('user-status btn btn-primary')).click();
+    await driver.wait(until.elementLocated(By.linkText('Create your account')), 15000);
+    await driver.findElement(By.linkText('Create your account')).click();
+    await driver.wait(until.elementLocated(By.xpath('/html/body/div[6]/div/div/div[1]/div')), 15000);
+    let title = await driver.findElement(By.xpath('/html/body/div[6]/div/div/div[1]/div')).getText();
+    expect(title).toEqual("User Registration");
+});
+
+test('User can successfully create an account', async () => {
+    let driver = await setUpDriver();
+    await driver.findElement(By.className('user-status btn btn-primary')).click();
+    await driver.wait(until.elementLocated(By.linkText('Create your account')), 15000);
+    await driver.findElement(By.linkText('Create your account')).click();
+    await driver.wait(until.elementLocated(By.xpath("//input[@value='Create My Account']")), 15000);
+    await driver.findElement(By.id('signupEmail')).sendKeys('test@selenium');
+    await driver.findElement(By.id('signupPassword')).sendKeys('Test123!');
+    await driver.findElement(By.id('signupPasswordConfirmation')).sendKeys('Test123!');
+    await driver.findElement(By.id('signupOrganization')).sendKeys('Selenium');
+    await driver.findElement(By.xpath("//input[@value='Create My Account']")).click();
+});
+
+test('user can successfully open and Log into selenium test user', async () => {
+    let driver = await setUpDriver();
+    await driver.findElement(By.className('user-status btn btn-primary')).click();
+    await driver.findElement(By.id('loginUsername')).sendKeys('test@selenium');
+    await driver.findElement(By.id('loginPassword')).sendKeys('Test123!');
+    await driver.findElement(By.xpath("//input[@value='Login']")).click();
+    await driver.wait(until.elementLocated(By.className('user-status')));
+});
+
+test('user can see list of existing surveys once logged in', async () => {
+    let driver = await setUpDriver();
+    await driver.findElement(By.className('user-status btn btn-primary')).click();
+    await driver.findElement(By.id('loginUsername')).sendKeys('test@selenium');
+    await driver.findElement(By.id('loginPassword')).sendKeys('Test123!');
+    await driver.findElement(By.xpath("//input[@value='Login']")).click();
+    await driver.wait(until.elementLocated(By.className('card-header')));
+    let existingSurveys = await driver.findElement(By.className('card-header'));
+    expect(existingSurveys).toBeTruthy();
+})
+
+test('user can start a new survey without logging in', async () => {
+    let driver = await setUpDriver();
+    await driver.findElement(By.xpath("//*[contains(text(), 'Start New Survey')]")).click();
+    await driver.wait(until.urlContains("DesignAssistantSurvey"));
+    let currentUrl = await driver.getCurrentUrl();
+    expect(currentUrl).toContain("DesignAssistantSurvey");
+})
