f5bc6e2a8d287e698729339736c168d44490ab53
Micheal <mlnguyen@ualberta.ca>
2020-11-26 00:13:53 -0700

    made home page tests work on single driver, added more home page tests

--- frontend/src/integration_tests/HomePage.test.js
+++ frontend/src/integration_tests/HomePage.test.js
@@ -11,9 +11,17 @@ async function setUpDriver() {
     return driver;
 }
 
-test('Successfully open User Registration page', async () => {
-    let driver = await setUpDriver();
+var driver;
+
+beforeAll(async () => {
+    driver = await setUpDriver();
+});
+
+test('User can open User Registration page', async () => {
+    // open login screen
     await driver.findElement(By.className('user-status btn btn-primary')).click();
+
+    // transition to registration page
     await driver.wait(until.elementLocated(By.linkText('Create your account')), 15000);
     await driver.findElement(By.linkText('Create your account')).click();
     await driver.wait(until.elementLocated(By.xpath('/html/body/div[6]/div/div/div[1]/div')), 15000);
@@ -21,43 +29,42 @@ test('Successfully open User Registration page', async () => {
     expect(title).toEqual("User Registration");
 });
 
-test('User can successfully create an account', async () => {
-    let driver = await setUpDriver();
-    await driver.findElement(By.className('user-status btn btn-primary')).click();
-    await driver.wait(until.elementLocated(By.linkText('Create your account')), 15000);
-    await driver.findElement(By.linkText('Create your account')).click();
-    await driver.wait(until.elementLocated(By.xpath("//input[@value='Create My Account']")), 15000);
+test('User can create an account', async () => {
+    // fill out registration information and signup
     await driver.findElement(By.id('signupEmail')).sendKeys('test@selenium');
     await driver.findElement(By.id('signupPassword')).sendKeys('Test123!');
     await driver.findElement(By.id('signupPasswordConfirmation')).sendKeys('Test123!');
     await driver.findElement(By.id('signupOrganization')).sendKeys('Selenium');
     await driver.findElement(By.xpath("//input[@value='Create My Account']")).click();
+    driver.close();
 });
 
 test('user can successfully open and Log into selenium test user', async () => {
-    let driver = await setUpDriver();
+    driver = await setUpDriver();
+    // log into the created test user
     await driver.findElement(By.className('user-status btn btn-primary')).click();
     await driver.findElement(By.id('loginUsername')).sendKeys('test@selenium');
     await driver.findElement(By.id('loginPassword')).sendKeys('Test123!');
     await driver.findElement(By.xpath("//input[@value='Login']")).click();
-    await driver.wait(until.elementLocated(By.className('user-status')));
+    await driver.wait(until.elementLocated(By.className("usersettings-dropdown dropdown")));
 });
 
+
 test('user can see list of existing surveys once logged in', async () => {
-    let driver = await setUpDriver();
-    await driver.findElement(By.className('user-status btn btn-primary')).click();
-    await driver.findElement(By.id('loginUsername')).sendKeys('test@selenium');
-    await driver.findElement(By.id('loginPassword')).sendKeys('Test123!');
-    await driver.findElement(By.xpath("//input[@value='Login']")).click();
     await driver.wait(until.elementLocated(By.className('card-header')));
     let existingSurveys = await driver.findElement(By.className('card-header'));
     expect(existingSurveys).toBeTruthy();
-})
+});
 
-test('user can start a new survey without logging in', async () => {
-    let driver = await setUpDriver();
+test('User can log out of their account', async () => {
+    await driver.findElement(By.className('usersettings-dropdown dropdown')).click();
+    await driver.findElement(By.xpath("//*[contains(text(), 'Log Out')]")).click();
+});
+
+test('User can start a new survey without logging in', async () => {
     await driver.findElement(By.xpath("//*[contains(text(), 'Start New Survey')]")).click();
     await driver.wait(until.urlContains("DesignAssistantSurvey"));
     let currentUrl = await driver.getCurrentUrl();
     expect(currentUrl).toContain("DesignAssistantSurvey");
+    driver.close();
 })
