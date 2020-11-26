ddbd829f4e72bb88c0339f64d952e7140fd0c4f3
Erik <ligai@ualberta.ca>
2020-11-26 16:20:49 -0700

    test for getting all users

--- backend/test/api/users/get.js
+++ backend/test/api/users/get.js
@@ -52,7 +52,24 @@ describe('GET /users', () => {
         .catch((err) => done(err));
     });
 
+    // test getting all users with valid auth token
+    it('get all users by auth token returns all valid users', (done) => {
+        request(app).get('/users')
+        .set('x-auth-token', testToken)
+        .then((res) => {
+            const users = res.body;
 
+            // iterate through all returned users and assert they contain valid fields
+            for (let i = 0; i < users.length; ++i) {
+                expect(users[i]).to.contain.property('role');
+                expect(users[i]).to.contain.property('_id');
+                expect(users[i]).to.contain.property('email');
+                expect(users[i]).to.contain.property('username');
+            }
+            done();
+        })
+        .catch((err) => done(err));
+    });
 });
 
 describe('GET /users/user', () => {
