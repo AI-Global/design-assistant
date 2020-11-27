35928745c5f4e4dda9a11d0dce9e1538c5815adb
Micheal <mlnguyen@ualberta.ca>
2020-11-26 18:22:19 -0700

    added provider and resource api endpoint tests

--- backend/test/api/trustedairesources/trustedAiResourcesTest.js
+++ backend/test/api/trustedairesources/trustedAiResourcesTest.js
@@ -1,39 +1,74 @@
-/*
 require('dotenv').config();
 process.env.NODE_ENV = 'test';
 const expect = require('chai').expect;
 const request = require('supertest');
 const app = require('../../../app.js');
 
+var resourceID = "";
+
+describe('PUT /trustedairesources', () => {
+    it('add new trusted ai resource', (done) => {
+        request(app).put('/trustedairesources')
+            .send({
+                resource: "testresource",
+                description: "",
+                source: "test"
+            })
+            .then((res) => {
+                const body = res.body;
+                expect(body).to.contain.property('_id');
+                expect(body).to.contain.property('resource');
+                expect(body).to.contain.property('description');
+                expect(body).to.contain.property('source');
+                resourceID = body["_id"];
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+    });
+
+    it('update trusted ai resource', (done) => {
+        request(app).put('/trustedairesources/' + resourceID)
+            .send({
+                resource: "updatedTestresource",
+                description: "",
+                source: "test"
+            })
+            .then((res) => {
+                const body = res.body;
+                expect(body).to.contain.property('_id');
+                expect(body).to.contain.property('resource');
+                expect(body).to.contain.property('description');
+                expect(body).to.contain.property('source');
+                done();
+            })
+    });
+});
+
 describe('GET /trustedairesources', () => {
-    before((done) => {
-        connect()
-            .then(() => done())
-            .catch((err) => done(err));
-
-    });
-
-    after((done) => {
-        connect()
-            .then(() => done())
-            .catch((err) => done(err));
-
-    });
-
     // Test getting surveyJS questions JSON
-    it('Getting list of all trustedairesources', (done) => {
-        // TODO: Make post request once connected to mockdb
+    it('Getting all trusted ai resources', (done) => {
         request(app).get('/trustedairesources')
-        .then((res) => {
-            const body = res.body;
-            expect(body[0]).to.contain.property('_id');
-            expect(body[0]).to.contain.property('resource');
-            expect(body[0]).to.contain.property('description');
-            expect(body[0]).to.contain.property('source');
- 
-            done();
-        })
-        .catch((err) => done(err));
+            .then((res) => {
+                const body = res.body;
+                expect(body[0]).to.contain.property('_id');
+                expect(body[0]).to.contain.property('resource');
+                expect(body[0]).to.contain.property('description');
+                expect(body[0]).to.contain.property('source');
+
+                done();
+            })
+            .catch((err) => done(err));
     });
 
-});*/
+});
+
+describe('DELETE /trustedairesources', () => {
+    it('delete trusted ai resource by id', (done) => {
+        request(app).delete('/trustedairesources/' + resourceID)
+            .then((res) => {
+                done();
+            })
+            .catch((err) => done(err));
+    });
+});
