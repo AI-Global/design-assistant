a44b8e5c2cd089d7140c167786637a97f463991e
George Everitt <geveritt@appliedrelevance.com>
2022-03-08 10:09:04 -0500

    Added download_database.sh to scripts

--- scripts/download_database.sh
+++ scripts/download_database.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 # Dump the MongoDB Database to a local archive and upload it to local mongodb
-
-ARCHIVE=archive
-REMOTE_DB=rai-certification-1
-REMOTE_NS=rai-certification-1.*
-LOCAL_DB=rai-local-dev
-LOCAL_NS=rai-local-dev.*
-PASSWORD=XptbITzKsfyZe8zp
-USERNAME=OpromaAdmin
-REMOTE_URI=mongodb+srv://cluster0.krmr3.mongodb.net/
-LOCAL_HOST="localhost:27017"
-
+# WARNING: THIS WILL OVERWRITE THE TARGET DATABASE WITH EXTREME PREJUDICE
+source .env
 mongodump --archive=$ARCHIVE --uri $REMOTE_URI  --db $REMOTE_DB --username $USERNAME  --password $PASSWORD
 mongorestore --archive=$ARCHIVE --drop --host $LOCAL_HOST  --nsFrom=$REMOTE_NS --nsTo=$LOCAL_NS
