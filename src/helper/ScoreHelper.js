3f815d6675a59875cc4899a979cccd6ba9537046
George Everitt <geveritt@oproma.com>
2022-04-06 17:20:46 -0400

    Working prototype of converting SVG to PNG.

---
+++ src/helper/ScoreHelper.js
@@ -0,0 +1,36 @@
+// Compute Scores
+//
+// There are two assessments: AI System Level Assessment and the Organizational Maturity Assessment
+//
+// AI System Level Assessment:
+//
+// Questions are divided in to two categories: risk and mitigation.
+// Answer choices have a score range of -50 to 50 points.
+// -50 to 0 is for risk.
+// 0 to 50 is for mitigation.
+// All of the questions in the assessment follow that rule.
+// All of the questions are grouped into subdimensions.
+// Subdimensions are grouped into dimensions.
+// Each subdimension and dimension has a weight.
+// All of the subdimension weights total to the dimension weight.
+// Each question has several answer choices.
+// Range for each question 
+//
+
+// Each of these assessments follow the same rules.
+// Caveat 1: the organizational maturity assessment is not broken into risk and mitigation categories.  
+// Caveat 2: the organizational maturity assessment is not broken into subdimensions.
+// 
+// The overall certification level score is 50% AI System Level Assessment and 50% Organizational Maturity Assessment.
+// The range of the overall certification level score is 0 to 200, with each assessment worth 100 points.
+// Caveat 3: sometimes the organization uses multiple AI systems.  In that case the assigned weight for the AI System 
+//           Level Assessment is divided by the number of AI systems assessments.
+// The final score will determine what certification level you will get.
+//  0-99 Not Certified
+//  100-119 Certified 
+//  120-139 Certified Silver
+//  140-159 Certified Gold
+//  160+ Certified Platinum
+
+
+
